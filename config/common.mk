afd3e4e732e79dc439b981fec2525c338e22f587
whyakari <barryofc11@gmail.com>
2026-08-12 09:23:49 -0300

    Revert "config: Exclude prebuilts/misc/protobuf_vendorcompat"
    this broken for now, then revert
    
    This reverts commit 5e9ff420e7bc05e04700cbae8b1230e30b58850b.

--- config/common.mk
+++ config/common.mk
@@ -1,9 +1,8 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-# Exclude repos from bp scanning
+# Exclude kernel platform repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
-PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
